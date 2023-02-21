9295df7d98dd125331528bc81538aee609a8a73a
jacobfredriksson <jacobfredriksson@live.se>
2023-02-21 15:12:12 +0100

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Jacob Fredriksson on 2023-02-21.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
